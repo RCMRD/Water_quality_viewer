9b00537f2ac6fc1f5d23b825718a05d506075b65
JamesWanjohi <jnyaga@rcmrd.org>
2022-04-13 00:59:54 +0300

    fixed the icon styling

--- query/static/query/js/query.js
+++ query/static/query/js/query.js
@@ -65,13 +65,14 @@ const app_control = (function() {
 
       return new ol.Map({
         controls: ol.control.defaults().extend(controls),
-        interactions: ol.interaction.defaults().extend([
-            new ol.interaction.Select({
-                condition: function(evt) {
-                    return evt.type == 'pointermove' || evt.type == 'singleclick';
-                },
-            })
-            ]),
+        interactions: ol.interaction.defaults(),
+        // .extend([
+        //     new ol.interaction.Select({
+        //         condition: function(evt) {
+        //             return evt.type == 'pointermove' || evt.type == 'singleclick';
+        //         },
+        //     })
+            // ]),
         layers: [raster],
         target: target,
         view: new ol.View({
@@ -259,15 +260,18 @@ const app_control = (function() {
             labelPoint: new ol.geom.Point(ol.proj.fromLonLat(point)),
             name: latlonarr.id
         });
-        // const iconStyle = new ol.style.Style({
-        //   image: new ol.style.Icon({
-        //     anchor: [0.5, 46],
-        //     anchorXUnits: 'fraction',
-        //     anchorYUnits: 'pixels',
-        //     src: static_url + .'/query/imgs/loc.jpg',
-        //   }),
-        // });
-        // feature.setStyle(iconStyle);
+        const iconStyle = new ol.style.Style({
+          image: new ol.style.Icon({
+            anchor: [0.5, 46],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            opacity: 0.75,
+            size: [715, 715],
+            src: static_url + 'query/imgs/loc.png',
+            scale: 0.1,
+          }),
+        });
+        feature.setStyle(iconStyle);
 
         return feature
     }
