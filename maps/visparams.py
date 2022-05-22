bbc4e65d188f1f23543377d2a63e83ca4946eabc
JamesWanjohi <jnyaga@rcmrd.org>
2022-05-22 17:18:41 +0300

    fixed bugs

--- maps/visparams.py
+++ maps/visparams.py
@@ -1,7 +1,7 @@
 lulcPalette = '#939393,#ffffbc,#d4ffff,#ff0000,#00eeee, \
                #0000ff,#ff9bbe,#ff82ff,#267300,#ffff00, \
                #cdcd00,#cd6839,#7c2800,#009600,#ffffff'
-vis={'chlor_a': {'min': 0, 'max': 50, 'palette': 'darkblue,blue,limegreen,yellow,orange,orangered,darkred'},
+vis={'chlor_a': {'min': 0, 'max': 60, 'palette': 'darkblue,blue,limegreen,yellow,orange,orangered,darkred'},
 'SD': {'min': 0, 'max': 2, 'palette': 'red,orangered,orange,yellow,limegreen,blue,darkblue'},
 'TSI': {'min': 0, 'max': 2, 'palette': 'red,orangered,orange,yellow,limegreen,blue,darkblue'},
 'TSI_R': {'min': 0, 'max': 2, 'palette': 'red,orangered,orange,yellow,limegreen,blue,darkblue'},
